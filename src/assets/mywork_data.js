45c1af69ee359815f734ace2d50ac7f0a2dea283
miecrose12 <itobiyeblossom75@gmail.com>
2025-09-27 20:58:48 +0100

    up

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,5 +1,5 @@
 import project1_img from '../assets/my project 4.png'
-import project2_img from '../assets/my project 3.png'
+import project2_img from '../assets/my project2.png'
 import project3_img from '../assets/my project1.png'
 
 const mywork_data = [
@@ -13,7 +13,7 @@ const mywork_data = [
         w_no:2,
         w_name:"Web design",
         w_img:project2_img,
-        w_link:"https://automart-kappa.vercel.app"
+        w_link:"https://nonsonet.vercel.app"
     },
     {
         w_no:3,
